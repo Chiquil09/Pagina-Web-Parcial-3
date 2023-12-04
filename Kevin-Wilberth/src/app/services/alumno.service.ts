bb2ab968714bcbc6d08e62073f1e2016f389e917
WilberthEC <wilberthechazarreta1606@gmail.com>
2023-12-04 18:27:22 -0500

    Actualizar Aprobado/Reprobado

--- Kevin-Wilberth/src/app/services/alumno.service.ts
+++ Kevin-Wilberth/src/app/services/alumno.service.ts
@@ -12,7 +12,8 @@ export class AlumnoService {
 
   private url = 'https://instituto-56503-default-rtdb.firebaseio.com';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    ) { }
 
   crearAlumno(alumno: AlumnoModel) {
 
@@ -26,6 +27,37 @@ export class AlumnoService {
   }
 
   actualizarAlumno( alumno: AlumnoModel ) {
+
+    function calcularPromedio() {
+      const cal1 = parseFloat((document.getElementById('cal1') as HTMLInputElement).value);
+      const cal2 = parseFloat((document.getElementById('cal2') as HTMLInputElement).value);
+      const cal3 = parseFloat((document.getElementById('cal3') as HTMLInputElement).value);
+
+      // Calcular el promedio
+      const promedio = (cal1 + cal2 + cal3) / 3;
+
+      let estado = "";
+
+      if (promedio >= 6 && promedio <= 10){
+        estado = "Aprobado"
+      } else {
+        estado = "Reprobado"
+      }
+
+
+      (document.getElementById('promedio') as HTMLInputElement).value = promedio.toFixed(2);
+      return estado;
+    }
+
+      const calcularButton = document.getElementById('calPromedio');
+      if (calcularButton) {
+        calcularButton.addEventListener('click', calcularPromedio);
+      }
+
+      calcularPromedio();
+      alumno.aprobado_reprobado = calcularPromedio()
+
+
 
     const alumnoTemp = {
      ...alumno
